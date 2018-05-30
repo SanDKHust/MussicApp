c2a13165194e239406f8a2678414c22218f89ff0
SanDK <khacsan.hust@gmail.com>
2018-05-30 08:39:48 +0700

    create new branch - commit + push

--- app/src/main/java/vn/edu/hust/soict/khacsan/musicapp/MainActivity.java
+++ app/src/main/java/vn/edu/hust/soict/khacsan/musicapp/MainActivity.java
@@ -15,12 +15,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         EditText editText = findViewById(R.id.edt);
+        
+        // test create new branch
 
-<<<<<<< HEAD
-=======
-        //$ git revert HEAD~1..HEAD | undo commit + push rồi
-
->>>>>>> parent of 2cd91ea... test undo commit + push
         final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
         alertDialogBuilder.setTitle("Thoat khoi chuong trinh");
         alertDialogBuilder.setMessage("Ban co thu");
